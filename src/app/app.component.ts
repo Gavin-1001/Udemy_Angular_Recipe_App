a85272b938f87f8d840ac4108c8c30a284356571
Gavin Shelley <40336736+Gavin-1001@users.noreply.github.com>
2022-09-16 13:02:37 +0100

    Added routes to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe_app';
+
+  loadedFeature = 'recipe' // stores which feature should be displayed
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
